11dd42bc0f9fa1407c2592d88c41f192cd966ce8
Alexandre Six Belem <alexandrenilton@gmail.com>
2019-11-21 11:30:51 -0300

    first commit

---
+++ src/README.md
@@ -0,0 +1,3 @@
+# Thymeleaf - Thymeaf / Spring
+
+Source code in this repo is to show how to use thymeleaf in a Springboot application
---
+++ src/test/java/education/scthymeleaf/ScThymeleafApplicationTests.java
@@ -0,0 +1,16 @@
+package education.scthymeleaf;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ScThymeleafApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
